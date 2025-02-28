74553513517bb37609ef2253c138ec006767f067
SamBSalgado <samuel.becerra@encora.com>
2025-02-28 13:34:22 -0600

    remove unnecessary comments

--- TypeScript/Problem1.ts
+++ TypeScript/Problem1.ts
@@ -1,27 +1,9 @@
 /*
-MEDIAN OF TWO SORTED ARAYS
+4. MEDIAN OF TWO SORTED ARAYS
 
 Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
 The overall run time complexity should be O(log (m+n)).
-
-Example 1:
-Input: nums1 = [1,3], nums2 = [2]
-Output: 2.00000
-Explanation: merged array = [1,2,3] and median is 2.
-
-Example 2:
-Input: nums1 = [1,2], nums2 = [3,4]
-Output: 2.50000
-Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
-
-
-Constraints:
-nums1.length == m
-nums2.length == n
-0 <= m <= 1000
-0 <= n <= 1000
-1 <= m + n <= 2000
--106 <= nums1[i], nums2[i] <= 106
+Go to LeetCode and look for the problem #4, and read the examples for a better understanding.
 */
 
 //                         +-------------------------------- NOTA: FALTA VER SI CUMPLE CON 0(log(m+n)).--------------------------------+
