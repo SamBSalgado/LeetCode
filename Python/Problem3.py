eaa16bd78f4cd5e89550648f227430d7cb41851b
cramirez-encora <cecilia.ramirez@encora.com>
2025-03-01 21:57:20 -0600

    problem3 all test case passed

--- Python/Problem3.py
+++ Python/Problem3.py
@@ -1,16 +1,14 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        unrepeated = []
-        counter = 0
-        score = 0
-        for letter in s:            
-            if letter in unrepeated:
-                unrepeated = []
-                counter = 0
-            else:
-                unrepeated.append(letter)
-                counter += 1
-            if counter > score:
-                score = counter
-    
-        return score
+        char_index = {}
+        left = 0
+        max_length = 0  
+
+        for right in range(len(s)):  
+            if s[right] in char_index:  # If the character has been seen before, move left pointer
+                left = max(left, char_index[s[right]] + 1)  
+
+            char_index[s[right]] = right  # Store or update character index
+            max_length = max(max_length, right - left + 1)  
+
+        return max_length
